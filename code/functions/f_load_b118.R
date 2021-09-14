18628cf9d264301a501e656eb16f1415477bb6d1
Rich Pauloo <richpauloo@gmail.com>
2021-09-13 17:39:55 -0700

    generalize to any input

--- code/functions/f_load_b118.R
+++ code/functions/f_load_b118.R
@@ -1,6 +1,18 @@
 # load GSA boundaries
-f_load_b118 <- function(path){
+f_load_b118_basin <- function(path, basin = NULL, names = NULL){
+
   b <- sf::st_read(path) %>% 
     sf::st_transform(epsg)
+  
+  if(!is.null(basin)){
+    b <- filter(b, Basin_Su_1 %in% basin)
+  }
+  
+  if(is.null(basin)){
+    b <- pull(b, Basin_Su_1) %>% 
+      sort() %>% 
+      glue::glue_collapse(sep = "\n ")
+  }
+  
   return(b)
 }
