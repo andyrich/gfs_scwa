03cf7f369271698e8a129921d565cf8953fc6d34
RichPauloo <richpauloo@gmail.com>
2022-01-12 17:15:30 -0800

    run all database and rmd

--- code/run.R
+++ code/run.R
@@ -1,7 +1,4 @@
-f <- fs::dir_ls(here::here("code"), regexp = "03|04")
-keep <- c("f", "i", "st", "data_path", "epsg",
-          "f_load_b118_basin", "f_load_dot_env", 
-          "f_progress", "f_verify_non_duplicates")
+f <- fs::dir_ls(here::here("code"), regexp = "02|03|04")
 
 st <- Sys.time()
 for(i in seq_along(f)){
@@ -13,6 +10,9 @@ for(i in seq_along(f)){
   cat("\n\nCOMPLETED:", basename(f[i]), "\n\n")
   
   # clear all objects and packages, but keep the files to run, counter, and time
+  keep = c("f", "i", "st", "data_path", "epsg",
+           "f_load_b118_basin", "f_load_dot_env", 
+           "f_progress", "f_verify_non_duplicates")
   obj = ls(all = TRUE)
   obj = obj[-which(obj %in% keep)]
   pkg = paste('package:', names(sessionInfo()$otherPkgs), sep = "")
@@ -22,3 +22,14 @@ for(i in seq_along(f)){
   
 }
 Sys.time() - st
+
+# render markdown report
+rmarkdown::render(
+  here::here("code/06_report.Rmd"), 
+  output_file = here::here("code/06_report.html")
+)
+
+rmarkdown::render(
+  here::here("code/07_sanity_check.Rmd"), 
+  output_file = here::here("code/07_sanity_check.html")
+)
