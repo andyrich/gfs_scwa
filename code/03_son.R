161fae3a9a3b5a6ca616f6727b2d0591739c030e
andyrich <arich@scwa.ca.gov>
2022-03-28 15:11:12 -0700

    added more use types to parcel types that are added to group of schools/golf courses. then added computations that account for surface water/recycled water supplies to deduct from water use. added school_golf_course_recycled_water_use_ac_ft. updated references to updated schema. potentially closes #16

--- code/03_son.R
+++ code/03_son.R
@@ -25,7 +25,7 @@ psrp <- read_rds(path(data_path, "data_output/srp_parcel.rds"))
 cat("Loaded preprocedded spatial parcels from Sonoma County.\n")
 
 # final fields to use
-fields <- path(data_path, "schema/2021_11_03_schema.xlsx") %>% 
+fields <- path(data_path, "schema/2022_03_28_schema.xlsx") %>% 
   readxl::read_xlsx(sheet = 2, range = cellranger::cell_cols("D")) %>% 
   set_names("name") %>% 
   filter(!is.na(name)) %>% 
@@ -570,10 +570,10 @@ pson<- left_join(pson, lawn_per_apn) %>%
 pson <- pson %>% 
   mutate(School_Golf_GW_Use_Prelim_Ac_Ft = 
            ifelse(str_detect(tolower(UseCode_Description), 
-                             "school|golf|country club"),
-                  # aw*LandSizeAcres*0.5, 0),
-                  aw*lawn_acres, 0),)   %>%
-  select(-lawn_acres)
+         "school|golf|country club|winery|cemeter|city park|county park|privately owned park|business park common area"),
+         # aw*LandSizeAcres*0.5, 0),
+         aw*lawn_acres, 0),)   %>%
+           select(-lawn_acres)
 # # following meeting with Marcus, Rob, and Andy: schools with
 # # a public water connection are assumed to NOT draw from groundwater
 # School_Golf_GW_Use_Prelim_Ac_Ft = 
@@ -586,6 +586,39 @@ pson <- pson %>%
 #   ifelse(str_detect(tolower(UseCode_Description), "golf|country club") &
 #            Onsite_Well == "No",
 #          0, School_Golf_GW_Use_Prelim_Ac_Ft)
+
+# Code to use to incorporate surface water/recycled water uses.
+# calculate groundwater School_Golf_GW_Use_Prelim_Ac_Ft water use
+pson <- pson %>%
+  mutate(
+    # first ensure that NA values in water budget components go to 0
+    School_Golf_GW_Use_Prelim_Ac_Ft  = ifelse(
+      is.na(School_Golf_GW_Use_Prelim_Ac_Ft), 0, School_Golf_GW_Use_Prelim_Ac_Ft),
+    Surface_Water_Use_Ac_Ft  = ifelse(
+      is.na(Surface_Water_Use_Ac_Ft), 0, Surface_Water_Use_Ac_Ft),
+    Recycled_Water_Use_Ac_Ft = ifelse(
+      is.na(Recycled_Water_Use_Ac_Ft), 0, Recycled_Water_Use_Ac_Ft),
+    #calculate School_Golf_Surface_Recycled_Use_Ac_Ft
+    School_Golf_Surface_Recycled_Use_Ac_Ft = Surface_Water_Use_Ac_Ft + Recycled_Water_Use_Ac_Ft,
+    # Ag School_Golf_GW_Use_Prelim_Ac_Ft use is the following mass balance:
+    School_Golf_GW_Use_Prelim_Ac_Ft =
+      School_Golf_GW_Use_Prelim_Ac_Ft -
+      (Surface_Water_Use_Ac_Ft + Recycled_Water_Use_Ac_Ft))
+
+# if a parcel receives more water from surface and recycled sources
+# than estimated demand, the calculated groundwater use is negative, so
+# we coerce this to zero
+pson <- pson %>%
+  mutate(School_Golf_GW_Use_Prelim_Ac_Ft = ifelse(
+    School_Golf_GW_Use_Prelim_Ac_Ft < 0, 0, School_Golf_GW_Use_Prelim_Ac_Ft))
+
+# if a parcel receives more water from surface and recycled sources
+# than estimated demand, then set the
+# total School_Golf_Surface_Recycled_Use_Ac_Ft = School_Golf_GW_Use_Prelim_Ac_Ft
+pson <- pson %>%
+  mutate(School_Golf_Surface_Recycled_Use_Ac_Ft = ifelse(
+    School_Golf_Surface_Recycled_Use_Ac_Ft > School_Golf_GW_Use_Prelim_Ac_Ft,
+    School_Golf_GW_Use_Prelim_Ac_Ft, School_Golf_Surface_Recycled_Use_Ac_Ft))
 
 
 # # school locations
