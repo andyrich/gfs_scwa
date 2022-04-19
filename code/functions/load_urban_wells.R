9d8ba31ceda7c2996af7d72e031e55215e2f3d78
andyrich <arich@scwa.ca.gov>
2022-04-19 09:23:07 -0700

    updated load urban wells to reflect edits made by R. pennington to list of parcels. closes #25

--- code/functions/load_urban_wells.R
+++ code/functions/load_urban_wells.R
@@ -3,10 +3,10 @@ load_urban_wells <- function(data_path,parc){
   print('loading surface water data')
 
   urban_wells_list <- path(data_path, "general/urban_wells", 
-                   'urban_wells.csv')
+                   'urban_wells_ALL_2022_04_18.xlsx')
   print('Loading pre-processed list of urban wells')
   urb <- urban_wells_list %>%
-    st_read() %>%
+    readxl::read_xlsx(sheet = 'Urban_Wells_ALL_2022_04_18') %>%
     select(APN)
   
   urb$Urban_list <- 'Yes'
