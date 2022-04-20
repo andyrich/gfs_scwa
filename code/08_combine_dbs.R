96218e087c1b3ff3514924581672910c52097552
andyrich <arich@scwa.ca.gov>
2022-04-20 13:11:34 -0700

    added function to reorder and re-name outputs for PRMD. uses fields named: Field_Name and Field_Name_PRMD issue #30

--- code/08_combine_dbs.R
+++ code/08_combine_dbs.R
@@ -30,18 +30,34 @@ all %>%
 gjson_out <- path(data_path, "data_output/soco_gsas_parcel.geojson")
 shp_out   <- path(data_path, "data_output/shp/soco_gsas_parcel.shp")
 
+
+print('writing geojson')
 if(file_exists(gjson_out)) file_delete(gjson_out)
 st_write(all, gjson_out)
 
-if(file_exists(shp_out)) file_delete(shp_out)
-st_write(all, shp_out)
+# print('writing shapefile')
+# if(file_exists(shp_out)) file_delete(shp_out)
+# st_write(all, shp_out)
 
 
-# swap field names with SCI field names and write
-sci <- read_csv(path(data_path, "general/sci_key.csv"))
-names(all) <- sci$new[match(names(all), sci$old)]
+# write prmd formatted files
+all <- relable_parc(all)
+# write to shp and csv
+all %>% 
+  st_drop_geometry() %>% 
+  write_csv(path(data_path, "data_output/soco_gsas_parcel_prmd.csv"))
 
-shp_sci_out <- path(data_path, "data_output/shp/soco_gsas_parcel_sci.shp")
+shp_out_prmd   <- path(data_path, "data_output/shp/soco_gsas_parcel_prmd.shp")
+if(file_exists(shp_out_prmd)) file_delete(shp_out_prmd)
+st_write(all, shp_out_prmd)
 
-if(file_exists(shp_sci_out)) file_delete(shp_sci_out)
-st_write(all, shp_sci_out)
+
+# # swap field names with SCI field names and write
+# sci <- read_csv(path(data_path, "general/sci_key.csv"))
+# names(all) <- sci$new[match(names(all), sci$old)]
+# 
+# shp_sci_out <- path(data_path, "data_output/shp/soco_gsas_parcel_sci.shp")
+# print('writing sci shapefile')
+# if(file_exists(shp_sci_out)) file_delete(shp_sci_out)
+# st_write(all, shp_sci_out)
+# print('done writing shapefile')
---
+++ code/functions/database_relabeler.R
@@ -0,0 +1,48 @@
+# load GSA boundaries
+relable_parc <- function(parc){
+  print('loading database names')
+  
+  s_ <- path(data_path, "schema", 
+                           'schema_layout_temp.xlsx')
+  print('Loading pre-processed list of urban wells')
+  schema <- s_ %>%
+    readxl::read_xlsx(sheet = 'Sheet1') %>%
+    select(c('Field_Name', 'Field_Name_PRMD'))
+  
+  new_row <- c('geometry', 'geometry')
+  schema <- rbind(schema, new_row)
+  # print('here is the schema file')
+  # print(schema)
+  # print('\n')
+  # print('colnames from parc before changing names')
+  # print(colnames(parc))
+  print('Done loading schema')
+  
+  # # swap field names with field_names_prmd
+  names(parc) <- schema$Field_Name_PRMD[match(names(all), schema$Field_Name)]
+  print('Done re-naming parcel layer')
+  
+  nonmatches <- schema$Field_Name_PRMD[!(schema$Field_Name_PRMD %in% colnames(parc))]
+  print('these are fields that are missing from the schema')
+  print(nonmatches)
+  
+  # print('colnames from parc after changing names')
+  # print(colnames(parc))
+  # print('Done loading schema')
+  
+  #reorder columns
+  parc <- parc[,schema$Field_Name_PRMD]
+  print('Done re-ordering parcel layer')
+  
+  # print(colnames(parc))
+  
+  # # swap field names with SCI field names and write
+  # sci <- read_csv(path(data_path, "general/sci_key.csv"))
+  # names(all) <- sci$new[match(names(all), sci$old)]
+
+  
+  return(parc)
+}
+
+
+
