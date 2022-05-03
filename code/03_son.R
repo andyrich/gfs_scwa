832adad6939452aa7a61f5a152cd44e98a6fd177
andyrich <arich@scwa.ca.gov>
2022-05-03 13:23:50 -0700

    code/00_muni_water_use.R

--- code/03_son.R
+++ code/03_son.R
@@ -32,8 +32,8 @@ psrp <- read_rds(path(data_path, "data_output/srp_parcel.rds"))
 cat("Loaded preprocedded spatial parcels from Sonoma County.\n")
 
 # final fields to use
-fields <- path(data_path, "schema/2022_03_28_schema.xlsx") %>% 
-  readxl::read_xlsx(sheet = 2, range = cellranger::cell_cols("D")) %>% 
+fields <- path(data_path, "schema/GSA Schema 20220503.xlsx") %>% 
+  readxl::read_xlsx(sheet = 1, range = cellranger::cell_cols("B")) %>% 
   set_names("name") %>% 
   filter(!is.na(name)) %>% 
   pull(name)
