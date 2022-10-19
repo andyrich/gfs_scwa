8bc774e76c6ac24fba88d3e62cd8e8c3eb38f308
Andrew Rich <arich@scwa.ca.gov>
2022-10-19 11:48:59 -0700

    commiting situs, jurisdiction changes adding Sonoma Water service areas

--- code/08_combine_dbs.R
+++ code/08_combine_dbs.R
@@ -51,6 +51,9 @@ all$GSA_Jurisdiction_Modified <-'No'
 all$GSA_Jurisdiction_Mod_Value <-NA
 all$GSA_Jurisdiction <-all$GSA_Jurisdiction_Prelim
 
+Parcel_fee
+Fee_Rate
+Parcel_Fee_Subsidized
 
 
 # write to shp and csv
